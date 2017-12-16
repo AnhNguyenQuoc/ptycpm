c9dd1c037818c93b7f19c5e00b33de2cf85223cf
Anh Nguyen <nguyenquocanh121296@gmail.com>
2017-12-16 17:02:50 +0700

    Change admin User

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,2 @@
 
-User.create({:name=>"anh", :email => "anh@gmail.com", :password => "123456", :password_confirmation => "123456", :phone => "01202917176", :address => "54/6/8 Hung vuong", :role => true})
-User.create({:name=>"nghia", :email => "nghia@gmail.com", :password => "123456", :password_confirmation => "123456", :phone => "01202917176", :address => "54/6/8 Hung vuong", :role => false})
+User.create({:name=>"admin", :email => "admin@gmail.com", :password => "123456", :password_confirmation => "123456", :phone => "090000000", :address => "Earth", :role => true})
